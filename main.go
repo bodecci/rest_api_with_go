40bd6ace3b579e54986c391619f5409665f8c369
Bode Falade <bbfalade@spscommerce.com>
2019-07-02 10:15:30 -0500

    added the handler functios for the routes.

--- main.go
+++ main.go
@@ -21,13 +21,38 @@ type Author struct {
 	LastName  string `json:"lastname"`
 }
 
+// GET all Books
+func getBooks(w http.ResponseWriter, r *http.Request) { //similar to (req, res)
+
+}
+
+// to GET single Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// to create a new Book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// to update Book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// to delete a Book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	// init router
 	r := mux.NewRouter()
 
 	// route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
