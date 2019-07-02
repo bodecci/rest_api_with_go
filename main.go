164cd97cde6c939f7db1362818963621d5d5bf2e
Bode Falade <bbfalade@spscommerce.com>
2019-07-02 11:11:35 -0500

    add GET route to get all Books.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -12,7 +13,7 @@ type Book struct {
 	ID     string  `json:"id"`
 	ISBN   string  `json:"isbn"`
 	Title  string  `json:"title"`
-	Author *Author `json:"id"`
+	Author *Author `json:"author"`
 }
 
 // Author struct
@@ -21,9 +22,13 @@ type Author struct {
 	LastName  string `json:"lastname"`
 }
 
+// Init books var as a slice Book struct
+var books []Book
+
 // GET all Books
 func getBooks(w http.ResponseWriter, r *http.Request) { //similar to (req, res)
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 // to GET single Book
@@ -50,9 +55,14 @@ func main() {
 	// init router
 	r := mux.NewRouter()
 
+	//Mock data - @todo - implement DB
+	books = append(books, Book{ID: "1", ISBN: "448743", Title: "Book One",
+		Author: &Author{FirstName: "John", LastName: "Doe"}})
+	books = append(books, Book{ID: "2", ISBN: "448744", Title: "Book Two",
+		Author: &Author{FirstName: "Johnny", LastName: "Does"}})
 	// route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id", getBook).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
